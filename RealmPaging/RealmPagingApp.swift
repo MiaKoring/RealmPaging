ecc46a5adb607314ed62c4c14a443fd4f9263e4f
MiaKoring <143010553+MiaKoring@users.noreply.github.com>
2024-06-27 20:45:53 +0200

    Initial Commit

---
+++ RealmPaging/RealmPagingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmPagingApp.swift
+//  RealmPaging
+//
+//  Created by Mia Koring on 27.06.24.
+//
+
+import SwiftUI
+
+@main
+struct RealmPagingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
